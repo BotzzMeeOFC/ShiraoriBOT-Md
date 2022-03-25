9a9f1d9b07e98a6116c8e4831e0779fb9d885b14
ʙᴏᴛᴢᴢ ᴍᴇᴇ <101207430+BotzzMeeOFC@users.noreply.github.com>
2022-03-26 04:58:15 +0700

    Add files via upload

---
+++ plugins/donasi.js
@@ -0,0 +1,63 @@
+/**
+ * TOLONG JANGAN GANTI GAMBARNYA,NOMORKU DAN SAWERIAKU
+ * MENDING KALIAN TAMBAHIN NOMOR KALIAN
+*/
+
+const { default: makeWASocket, BufferJSON, WA_DEFAULT_EPHEMERAL, generateWAMessageFromContent, downloadContentFromMessage, downloadHistory, proto, getMessage, generateWAMessageContent, prepareWAMessageMedia } = require('@adiwajshing/baileys')
+let handler = async (m) => {
+let duit = `*────── 「 DONATE 」 ──────*
+
+Hai 👋
+Kalian bisa mendukung saya agar bot ini dengan:
+┌〔 Donasi 〕
+├🏧 083174508962 (Dana/GoPay)
+└────
+Berapapun donasi kalian akan sangat berarti 👍
+
+Arigatou!
+
+Contact person Owner:
+wa.me/6283174508962 (Owner)`
+let message = await prepareWAMessageMedia({ image: {url: 'https://telegra.ph/file/244b73251b9e614a3193a.jpg' }}, { upload: conn.waUploadToServer })
+     const template = generateWAMessageFromContent(m.chat, proto.Message.fromObject({
+     templateMessage: {
+         hydratedTemplate: {
+           imageMessage: message.imageMessage,
+           hydratedContentText: duit,
+           hydratedFooterText: wm,
+           hydratedButtons: [{
+             urlButton: {
+               displayText: 'Back To Menu',
+               url: '.menu'
+             }
+
+           },
+               {
+             callButton: {
+               displayText: 'Pulsa',
+               phoneNumber: '+62 897-9530-736'
+             }
+           },           
+               {
+             quickReplyButton: {
+               displayText: '🧒 Owner',
+               id: '.owner',
+             }
+
+           }]
+         }
+       }
+     }), { userJid: m.sender, quoted: m });
+    //conn.reply(m.chat, text.trim(), m)
+    return await conn.relayMessage(
+         m.chat,
+         template.message,
+         { messageId: template.key.id }
+     )
+}
+
+handler.help = ['donasi']
+handler.tags = ['info']
+handler.command = /^dona(te|si)|bagiduit$/i
+
+module.exports = handler
